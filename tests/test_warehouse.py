6fb042b2be7b0cca27fa374865b1aab9e0cc5e4c
Ekaterina Nikonova <ekaterina.v.nikonova@gmail.com>
2024-01-25 08:10:12 +0100

    Build a larger warehouse in the test

--- tests/test_warehouse.py
+++ tests/test_warehouse.py
@@ -11,5 +11,5 @@ class TestWarehouse(TestCase):
         self.assertTrue(np.allclose(warehouse.actions, warehouse.actions.T))
 
     def test_draw(self):
-        warehouse = Warehouse(width=5, height=4)
+        warehouse = Warehouse(width=15, height=14)
         warehouse.draw()
